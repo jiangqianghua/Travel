dcb821915f3a2177ff692d56747b913abf02732d
jiangqianghua <240437339@qq.com>
2019-01-20 21:09:48 +0800

    添加样式重置 添加解决1像素边框问题 添加解决点击延迟300毫秒的问题

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 样式重置
+import './assets/styles/reset.css'
+// 解决1像素边框的问题
+import './assets/styles/border.css'
+// 引入fastClick，解决点击延迟300毫秒执行的问题
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
