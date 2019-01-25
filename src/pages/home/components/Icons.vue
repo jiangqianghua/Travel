ae81dd888780bbf2601f0a237a6cfc42b97d3c1e
jiangqianghua <240437339@qq.com>
2019-01-25 23:02:30 +0800

    完成菜单

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption" >
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon"
+             v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '1001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点1'
+        },
+        {
+          id: '1002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点2'
+        },
+        {
+          id: '1003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点3'
+        },
+        {
+          id: '1004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点4'
+        },
+        {
+          id: '1005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点5'
+        },
+        {
+          id: '1006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点6'
+        },
+        {
+          id: '1007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点7'
+        },
+        {
+          id: '1008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点8'
+        },
+        {
+          id: '1009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点9'
+        },
+        {
+          id: '1010',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点10'
+        },
+        {
+          id: '1011',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点1211222331'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icons
+    overflow hidden
+    height 0
+    padding-bottom 50%  // 高是宽的一半
+    .icon
+      position relative
+      overflow hidden
+      height  0
+      float left
+      width 25%
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        .icon-img-content
+          display block
+          margin 0 auto
+          width 80%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        color: $darkTextColor
+        text-align center
+        ellipsis()
+</style>
